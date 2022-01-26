9012ba23f9673c68de766b253e6e3c63d6d0bee7
dong heon cho <davinnovation@gmail.com>
2022-01-27 01:46:01 +0900

    fix create tempfile bug in windows-linux

--- fsos/bucket_manager_test.py
+++ fsos/bucket_manager_test.py
@@ -1,6 +1,7 @@
 import unittest
 import tempfile
 import pathlib
+import os
 
 from fsos import bucket_manager as bm
 
@@ -43,14 +44,16 @@ class BMTests(unittest.TestCase):
 
     def test_file_manage(self):
         # Windows bug for TemporaryFile access - https://stackoverflow.com/questions/23212435/permission-denied-to-write-to-my-temporary-file
-        temp_file = tempfile.TemporaryFile(delete=False)
+        # temp_file = tempfile.NamedTemporaryFile(mode='w')
+        temp_file = os.path.join(tempfile.gettempdir(), os.urandom(24).hex())
+        open(temp_file, "x").close()
         # with tempfile.TemporaryDirectory() as temp_dir:
         temp_dir = str(pathlib.Path(pathlib.Path.home(), ".fsos"))
         TEST_BUCKET_NAME = "test_bucket"
         TEST_OBJECT_NAME = "test_object"
         bm.make_bucket(TEST_BUCKET_NAME)
         bm.put_filepath(TEST_BUCKET_NAME, TEST_OBJECT_NAME,
-                        temp_file.name, meta_info={"cls": [1, 2]})
+                        temp_file, meta_info={"cls": [1, 2]})
         self.assertEqual(pathlib.Path(
             temp_dir, TEST_BUCKET_NAME, TEST_OBJECT_NAME).exists(), True)
         self.assertEqual(bm.get_filepaths(TEST_BUCKET_NAME, temp_dir), [
@@ -59,8 +62,7 @@ class BMTests(unittest.TestCase):
         bm.remove_object(TEST_BUCKET_NAME, TEST_OBJECT_NAME)
         bm.remove_bucket(TEST_BUCKET_NAME)
 
-        temp_file.close()
-        pathlib.Path(temp_file.name).unlink()
+        pathlib.Path(temp_file).unlink()
 
         pathlib.Path(temp_dir, ".fsos").unlink()
         pathlib.Path(temp_dir).rmdir()
