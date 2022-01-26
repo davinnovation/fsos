651d82a80d4f55087b1ea0b40c1102031d958c4a
davinnovation <davinnovation@gmail.com>
2022-01-26 18:25:44 +0900

    update temp mkdir to latest

--- fsos/bucket_manager_test.py
+++ fsos/bucket_manager_test.py
@@ -7,7 +7,7 @@ from fsos import bucket_manager as bm
 
 class BMTests(unittest.TestCase):
     def test_bucket(self):
-        with tempfile.mkdtemp() as temp_dir:
+        with tempfile.TemporaryDirectory() as temp_dir:
             TEST_BUCKET_NAME = "test_bucket"
             bm.make_bucket(TEST_BUCKET_NAME, temp_dir)
             self.assertEqual(pathlib.Path(temp_dir, TEST_BUCKET_NAME).exists(), True)
